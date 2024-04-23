ba95117d64141ef3e386669b5716c2daf0b14b37
dhirajmobicloud <117283769+dhirajmobicloud@users.noreply.github.com>
2024-04-23 16:42:03 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <img src="https://cdn.dribbble.com/users/372006/screenshots/2325846/media/a091c2fdf00cfe108982ed8066acc925.gif" alt="" srcset="" />
         </div>
         <div >
-            <button className='bg-amber-300 text-black py-2 px-4 rounded-full cursor-pointer transition duration-0 hover:duration-150 ease-in-out'>Get Started...</button>
+            <button className='bg-amber-300 text-black py-2 px-4 rounded-full cursor-pointer transition duration-0 hover:duration-150 ease-in-out'>Get Started</button>
         </div>
     </div>
   )
